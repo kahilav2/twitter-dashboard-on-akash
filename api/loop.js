0a62014d2254f47cfad545dfd2f019f4a5e2478b
kahilav2 <47920464+kahilav2@users.noreply.github.com>
2022-01-11 09:30:09 +0900

    change loop logic so time doesnt slip

--- api/loop.js
+++ api/loop.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const db = require('./utils/db.js');
 const { sleep } = require('../utils/common');
 const constants = require('../utils/constants');
+const { performance } = require('perf_hooks');
 
 const LOOP_INTERVAL = 1000 * 60 * 60 * 24;
 
@@ -10,6 +11,7 @@ const watchedTwitterIDs = constants.general.twitterIDs;
 const TWITTER_KEY = process.env.TWITTER_KEY;
 
 const loop = async () => {
+  let startTime = performance.now()
   try {
     for (const { twitterID, tags } of watchedTwitterIDs) {
       console.log(`Updating ${twitterID} (${tags})`);
@@ -24,15 +26,18 @@ const loop = async () => {
       
       const row = db.prepare('INSERT INTO data_points(twitter_id, followers_count, date) VALUES (?, ?, CURRENT_TIMESTAMP)');
       row.run(twitterID, result.data.followers_count);
-
+      
       console.log(`${twitterID} updated successfully`);
-      await sleep(8000);
+      await sleep(8000 / 100);
     }
   } catch (error) {
     console.error(error);
   }
-
-  setTimeout(loop, LOOP_INTERVAL);
+  let endTime = performance.now()
+  let timeElapsed = endTime - startTime;
+  console.log("timeElapsed", timeElapsed);
+  console.log("next update", LOOP_INTERVAL - timeElapsed)
+  setTimeout(loop, LOOP_INTERVAL - timeElapsed );
 }
 
 module.exports = loop
